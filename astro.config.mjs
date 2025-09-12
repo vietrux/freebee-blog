ce3fdba7348fc53426922b9b83b74fe9cbeba27e
Your Name <you@example.com>
2025-09-12 13:28:02 +0700

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://freebee.v1.io.vn',
   vite: {
       plugins: [tailwindcss()],
-	},
+    },
 
-  integrations: [],
+  integrations: [sitemap()],
 });
